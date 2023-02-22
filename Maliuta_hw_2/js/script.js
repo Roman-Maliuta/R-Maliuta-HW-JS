16a2d06402e6b9cdcf8a541bc35513bce014126c
Roman Maliuta <maluta.roma95@gmail.com>
2023-02-23 01:27:26 +0200

    hw2

---
+++ Maliuta_hw_2/js/script.js
@@ -0,0 +1,13 @@
+let numbreOne = Number(prompt('Input first number?'));
+
+let numbreTwo = Number(prompt('Input second number?'));
+
+console.log('Calculations are finished!');
+
+console.log('Sum: ' + numbreOne + ' + ' + numbreTwo + ' = ' + (numbreOne + numbreTwo));
+
+console.log('Diff: ' + numbreOne + ' - ' + numbreTwo + ' = ' + (numbreOne - numbreTwo));
+
+console.log('Mult: ' + numbreOne + ' * ' + numbreTwo + ' = ' + (numbreOne * numbreTwo));
+
+console.log('Div: ' + numbreOne + ' / ' + numbreTwo + ' = ' + (numbreOne / numbreTwo));
