b688b9c2d16291f6dd102d829915a3463c55430d
Roman Maliuta <maluta.roma95@gmail.com>
2023-02-23 01:21:24 +0200

    first commit

---
+++ Maliuta_hw_1/js/script.js
@@ -0,0 +1,10 @@
+alert('Hello!');
+
+const userName = prompt('What is your name?');
+
+const userAge = 2023 - Number(prompt("What's your birth year?"));
+
+const userCountry = prompt('Where are you from?');
+
+alert('Nice to meet you, ' + userName + '! You are ' + userAge + 
+ ' years old! You are from ' + userCountry + '!');
